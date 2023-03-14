f85b9574bd8a4cbe660b22f9aada2ccd640e7f5a
thomas <thomas@emotion.nl>
2023-03-14 12:12:14 +0100

    Added /add form + axios clients select

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../routes/Home';
+import Add from '../routes/Add';
 
 export const routes = createBrowserRouter([{
     path: "/",
     element: <Home />
+}, {
+    path: "/add",
+    element: <Add />
 }]);
 
 export default routes;
---
+++ src/routes/Add.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import './../App.css';
+import { Layout } from '../components/layout';
+import axios from 'axios';
+import { load } from 'cheerio';
+
+const formClasses = 'bg-gray-800 text-white rounded-md p-2 w-full mb-4';
+
+function Home() {
+
+  type Client = {
+    name: string;
+    color: string;
+  };
+
+  const [clients, setClients] = useState<Client[]>([]);
+  const [error, setError] = useState<any>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+
+    async function getClients() {
+      /**
+       * Get clients from API
+       * IMPORTANT: This is a CORS proxy, so it's not safe to use in production,
+       * read more here https://allorigins.win/.
+       */
+      await axios.get('https://api.allorigins.win/raw?url=https://pastebin.com/raw/zSFTiVWr', { timeout: 2000 })
+        .then((response) => {
+          console.log(response.data);
+          setClients(response.data);
+          setIsLoaded(true);
+        })
+        .catch((error) => {
+          setIsLoaded(true);
+          setError('Unable to fetch clients');
+        });
+    };
+
+    getClients();
+  }, []);
+
+  return (
+    <Layout>
+      <div className='md:m-auto md:w-4/6 lg:m-auto lg:w-3/6'>
+        <h3 className="text-lg mb-4">Add new password</h3>
+        <form>
+          <input placeholder='Title' className={formClasses} type='text' />
+          <input placeholder='Password' className={formClasses} type='password' />
+          <select className={formClasses} disabled={!isLoaded}>
+            {isLoaded && !error ? (<>
+              <option value=''>Select client</option>
+              {clients.map((client, index) => (
+                <option key={index} value={client.name}>{client.name}</option>
+              ))}
+            </>
+            ) : (
+              <>
+                {error ? (<option value='error'>{error}</option>) : (<option value=''>Loading...</option>)}
+              </>
+            )}
+          </select>
+          <button disabled={!isLoaded} type='submit' className='bg-gray-400 text-black rounded-md p-2'>Add</button>
+        </form>
+      </div>
+    </Layout >
+  );
+}
+
+export default Home;
