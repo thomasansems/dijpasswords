a08078516d0802990cd6b27723d3d8c39c50be36
thomas <thomas@emotion.nl>
2023-03-16 09:52:21 +0100

    Adding basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './routes/Home';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import Add from './routes/Add';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Add', () => {
+  it('should add a new password item when form is submitted', async () => {
+
+    render(<Add />);
+
+    // Fill out the form
+    const titleInput = screen.getByPlaceholderText('Title');
+    fireEvent.change(titleInput, { target: { value: 'Test title' } });
+
+    const passwordInput = screen.getByPlaceholderText('Password');
+    fireEvent.change(passwordInput, { target: { value: 'Test password' } });
+
+    const clientSelect = screen.getByLabelText('Client');
+    fireEvent.change(clientSelect, { target: { value: 'Client 1' } });
+
+    // Submit the form
+    const addButton = screen.getByText('Add');
+    fireEvent.click(addButton);
+
+  });
 });
